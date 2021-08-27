8f0f229c446144744c996f7f3e6ec40c80703d9c
Nikita Popov <barbaroso1346@gmail.com>
2021-08-28 03:21:02 +0700

    departments filter

--- packages/web/src/app.tsx
+++ packages/web/src/app.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import {Provider as ReduxProvider, useSelector} from 'react-redux'
 import store from "./store";
+import {DepartmentsTree} from "./components/DepartmentsTree/DepartmentsTree";
+import '../src/styles/index.scss'
 
 const App = () => {
+
+
+
+
   return <BrowserRouter>
     <ReduxProvider store={store}>
-      <div>APP</div>
+      <DepartmentsTree/>
     </ReduxProvider>
   </BrowserRouter>
 };
