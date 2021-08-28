f33b2e3543953ead7d26ab1296b19590cf7ea1b5
kiri <kirillknizhov@mail.ru>
2021-08-28 16:19:01 +0700

    breaking tasks, current day

--- packages/web/src/shedule-manager/index.ts
+++ packages/web/src/shedule-manager/index.ts
@@ -5,19 +5,20 @@ export type SheduleItem = {
   dueDate: number
   time: number
   isBreakable?: boolean
+  part?: number
 }
 export type task = { id: string, dueDate: number, time: number, isBreakable?: boolean }
 export type Machine = { tasks: SheduleItem[] }
 export type Shedule = { [key: string]: Machine }
 export const addTask = (machines: { [key: string]: Machine }, task: task, currentDate) => {
-  const [key, space] = getMinimalMachine(Object.entries(machines), task.dueDate)||[null,null]
-  console.log(key,task.dueDate,task.time)
+  const [key, space] = getMinimalMachine(Object.entries(machines), task.dueDate, currentDate) || [null, null]
+  console.log(key, task.dueDate, task.time)
   if (!key) {
     return null
   }
-  if (isMachineCanApplyTask(machines[key], task)) {
-    const appliedMachine = applyTaskToMachine(machines[key], task)
-    return {...machines,[key]:appliedMachine}
+  if (isMachineCanApplyTask(machines[key], task, currentDate)) {
+    const appliedMachine = applyTaskToMachine(machines[key], task, currentDate)
+    return {...machines, [key]: appliedMachine}
 
   } else {
     const nextMachines = {...machines}
@@ -33,25 +34,32 @@ export const addTask = (machines: { [key: string]: Machine }, task: task, curren
 
 }
 
-const getMinimalMachine = (machines: [string, Machine][], date) => {
+const getMinimalMachine = (machines: [string, Machine][], date, currentDate) => {
   return machines.map(([key, machine]) => {
-    let spaces = machine.tasks.filter(it => it.dueDate > date).map(it => {
+    let [beforeTasks, forwardTasks] = getForwardAndBeforeTasks(machine.tasks, currentDate)
+
+    let spaces = forwardTasks.filter(it => it.dueDate > date).map(it => {
       return it.dueDate - (it.end)
     })
-    const tasksBefore = machine.tasks.filter(it => it.dueDate <= date)
-    const startTask = tasksBefore.length ? tasksBefore[tasksBefore.length - 1].end : 0
-    console.log(key,startTask,date,date-startTask)
-    if(spaces.length){
-      return [key,Math.min(startTask+Math.min(...spaces),date) - startTask]
+    const tasksBefore = forwardTasks.filter(it => it.dueDate <= date)
+    const startTask = tasksBefore.length ? tasksBefore[tasksBefore.length - 1].end : currentDate
+    if (spaces.length) {
+      return [key, Math.min(startTask + Math.min(...spaces), date) - startTask, forwardTasks.reduce((acc, it) => {
+        return acc + it.time
+      },0)]
     }
-    return [key, date-startTask]
-  }).sort((a, b) => a[1] < b[1] ? 1 : a[1] === b[1] ? 0 : -1)[0]
+    return [key, date - startTask, forwardTasks.reduce((acc, it) => {
+      return acc + it.time
+    }, 0)]
+  }).sort((a, b) => a[1] < b[1] ? 1 : a[1] === b[1] ? a[2] > b[2] ? 1 : a[2] === b[2] ? 0 : -1:-1)[0]
 }
-const isMachineCanApplyTask = (machine, task) => {
+const isMachineCanApplyTask = (machine: Machine, task, currentDate) => {
   const date = task.dueDate
-  const tasksBefore = machine.tasks.filter(it => it.dueDate <= date)
-  const startTask = tasksBefore.length ? tasksBefore[tasksBefore.length - 1].end : 0
-  let spaces = machine.tasks.filter(it => it.dueDate > date).map(it => {
+  let [beforeTasks, forwardTasks] = getForwardAndBeforeTasks(machine.tasks, currentDate)
+
+  const tasksBefore = forwardTasks.filter(it => it.dueDate <= date)
+  const startTask = tasksBefore.length ? tasksBefore[tasksBefore.length - 1].end : currentDate
+  let spaces = forwardTasks.filter(it => it.dueDate > date).map(it => {
     return it.dueDate - (it.end)
   })
   if (!spaces.length) {
@@ -59,13 +67,51 @@ const isMachineCanApplyTask = (machine, task) => {
   }
   return startTask + task.time <= task.dueDate && Math.min(...spaces) >= task.time
 }
-const applyTaskToMachine = (machine:Machine, task) => {
-  machine.tasks = [...machine.tasks, task].sort((a, b) => a.dueDate < b.dueDate ? -1 : a.dueDate === b.dueDate ? 0 : 1)
-  machine.tasks.reduce((acc, it,index,array) => {
+const applyTaskToMachine = (machine: Machine, task, currentDate) => {
+  let [beforeTasks, forwardTasks] = getForwardAndBeforeTasks(machine.tasks, currentDate)
+
+  if (forwardTasks[0] && forwardTasks[0].start < currentDate && forwardTasks[0].end > currentDate) {
+    const splittedTask = [{
+      ...forwardTasks[0],
+      end: currentDate,
+      time: currentDate - forwardTasks[0].start
+    }, {...forwardTasks[0], start: currentDate, time: forwardTasks[0].end - currentDate}].map((it, index) => ({
+      ...it,
+      part: (it.part || 0) + index
+    }))
+    forwardTasks.shift()
+    forwardTasks = [splittedTask[1], ...forwardTasks]
+    beforeTasks = [...beforeTasks, splittedTask[0]]
+  }
+  forwardTasks = [...forwardTasks, task].sort((a, b) => a.dueDate < b.dueDate ? -1 : a.dueDate === b.dueDate ? 0 : 1)
+  forwardTasks.reduce((acc, it, index, array) => {
     it.start = acc
     it.end = acc + it.time
     return acc + it.time
-  },0)
+  }, currentDate)
+  machine.tasks = [...beforeTasks, ...forwardTasks]
   return machine
 }
-
+const getForwardAndBeforeTasks = (tasks, currentDate) => {
+  let forwardTasks = tasks.filter(it => it.end > currentDate)
+  let beforeTasks = tasks.filter(it => it.end <= currentDate) || []
+  if (forwardTasks[0] && forwardTasks[0].start < currentDate && forwardTasks[0].end > currentDate) {
+    const splittedTask = [{
+      ...forwardTasks[0],
+      end: currentDate,
+      time: currentDate - forwardTasks[0].start
+    }, {
+      ...forwardTasks[0],
+      start: currentDate,
+      time: forwardTasks[0].end - currentDate,
+      dueDate: forwardTasks[0].isBreakable ? forwardTasks[0].dueDate : forwardTasks[0].end
+    }].map((it, index) => ({
+      ...it,
+      part: (it.part || 0) + index
+    }))
+    forwardTasks.shift()
+    forwardTasks = [splittedTask[1], ...forwardTasks]
+    beforeTasks = [...beforeTasks, splittedTask[0]]
+  }
+  return [beforeTasks, forwardTasks]
+}
