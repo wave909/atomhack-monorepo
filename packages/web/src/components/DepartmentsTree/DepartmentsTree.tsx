879e3153e42fdcae2023c84883fb264d0e0caf0c
Nikita Popov <barbaroso1346@gmail.com>
2021-08-28 16:43:22 +0700

    Getting rid of moc file

--- packages/web/src/components/DepartmentsTree/DepartmentsTree.tsx
+++ packages/web/src/components/DepartmentsTree/DepartmentsTree.tsx
@@ -5,26 +5,28 @@ import style from './DepartmentTree.module.scss'
 import {Department} from "../DepartmentCard/department";
 import GraphScreen from "../../feature/graph";
 
-/* eslint import/no-webpack-loader-syntax: off */
-import StructureDot from '!!raw-loader!../../mock/structure.dot';
-
 export const DepartmentsTree = () => {
   const [chosenDepartments, setChosenDepartments] = useState<Department[]>([])
-  const [graphvizBuf, setGraphvizBuf] = useState('')
+  const [graphvizBuf, setGraphvizBuf] = useState('digraph G {}')
   useEffect( () => {
-    const extendedDepartments = chosenDepartments.slice(0, chosenDepartments.length)
+    const extendedDepartments = chosenDepartments.length === 0 ?
+      departmentList.slice(0, departmentList.length) : chosenDepartments.slice(0, chosenDepartments.length)
+
+    if(chosenDepartments.length !== 0){
       chosenDepartments.forEach(chosenDepartment => {
-      for(
-        let path = chosenDepartment.path.slice(0, chosenDepartment.path.length - 1);
-        path.length !== 0;
-        path = path.slice(0, path.length - 1)
-      ){
-        const foundDepartment = findByPath(path)
-        if(foundDepartment && !extendedDepartments.find(_department => _department.title === foundDepartment.title)){
-          extendedDepartments.push(foundDepartment)
+        for(
+          let path = chosenDepartment.path.slice(0, chosenDepartment.path.length - 1);
+          path.length !== 0;
+          path = path.slice(0, path.length - 1)
+        ){
+          const foundDepartment = findByPath(path)
+          if(foundDepartment && !extendedDepartments.find(_department => _department.title === foundDepartment.title)){
+            extendedDepartments.push(foundDepartment)
+          }
         }
-      }
-    })
+      })
+    }
+
     extendedDepartments.sort((a, b) => a.path.length > b.path.length ? 1 : -1)
 
     const newGraphvizBuf: any = []
@@ -62,7 +64,7 @@ export const DepartmentsTree = () => {
     />
 
     <div className={style['right-column']}>
-      <GraphScreen graphvizBuf={graphvizBuf === '' ? StructureDot : graphvizBuf}/>
+      <GraphScreen graphvizBuf={graphvizBuf}/>
     </div>
   </div>
 }
