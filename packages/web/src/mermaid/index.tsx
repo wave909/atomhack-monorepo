6570924fe999aab88d4022892d548a0952c9188d
kiri <kirillknizhov@mail.ru>
2021-08-28 15:01:51 +0700

    shedule manager demo

---
+++ packages/web/src/mermaid/index.tsx
@@ -0,0 +1,45 @@
+import React, {useEffect, useRef, useState} from "react"
+import mermaid from "mermaid"
+
+
+
+export const Mermaid = ({ chart }: {chart: string}) => {
+  mermaid.initialize({})
+  const ref = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if(!ref.current) return
+    delete ref.current.dataset.processed
+    mermaid.contentLoaded()
+
+    try {
+      const api = mermaid.parse(chart).parser.yy
+      if(api.graphType !== 'gantt') return
+      console.log(Object.keys(api))
+      console.log(api.getTasks()
+        .map(({startTime, endTime, task}) =>
+          `${task.trim()}: ${startTime.toISOString().substring(0, 10)} - ${endTime.toISOString().substring(0, 10)}`)
+        .join('\n'))
+    } catch (e) {
+      // ignored. mermaid-js throws errors when it cannot parse the input.
+    }
+  }, [chart])
+
+  if (!chart) return null
+  return (
+    <div className="mermaid" ref={ref}>
+      {chart}
+    </div>
+  )
+}
+
+const PlanConstructor = () => {
+  const [value, setValue] = useState("")
+  return <div>
+    <textarea value={value} onChange={(ev) => setValue(ev.target.value)}/>
+    <Mermaid chart={value}/>
+  </div>
+}
+
+export default PlanConstructor
+
---
+++ packages/web/src/shedule-manager/SheduleManager.tsx
@@ -0,0 +1,48 @@
+import React, {useEffect, useState} from "react";
+import {addTask, Shedule} from "./index";
+import {Mermaid} from "../mermaid";
+
+const today = new Date("1970-01-01T00:00:00.000Z")
+
+export const SheduleManager = () => {
+  const [currentDate, setCurrentDate] = useState(today.toISOString())
+  const [taskDate, setTaskDate] = useState(today.toISOString())
+  const [taskTitle, setTaskTitle] = useState("New")
+  const [taskTime, setTaskTime] = useState(0)
+  const [shedule, setShedule] = useState({one: {tasks: []}, two: {tasks: []}, three: {tasks: []}})
+  const [gantt, setGantt] = useState("")
+  useEffect(() => {
+    setGantt(convertSheduleToGantt(shedule))
+
+  }, [shedule])
+  console.log(gantt)
+  return <div>
+    <input value={currentDate} onChange={(e) => setCurrentDate(e.target.value)}/>
+    <input value={taskTitle} onChange={(e) => setTaskTitle(e.target.value)}/>
+    <input value={taskDate} onChange={(e) => setTaskDate(e.target.value)}/>
+    <input type={"number"} value={taskTime} onChange={(e) => setTaskTime(Number.parseInt(e.target.value))}/>
+    <button title={"Добавить задачу"} onClick={() => {
+      const curDate = new Date(currentDate)
+      const taskDueDate = new Date(taskDate)
+      const _taskTime = taskTime * 60 * 60 * 1000
+      console.log(taskDate, taskDueDate)
+      const newShedule = addTask(shedule, {
+        id: taskTitle,
+        dueDate: taskDueDate.getTime(),
+        time: _taskTime
+      }, curDate.getTime())
+      console.log(newShedule)
+      newShedule && setShedule(newShedule)
+    }}>Добваить задачу</button>
+    <Mermaid chart={gantt}/>
+  </div>
+}
+const convertSheduleToGantt = (shedule: Shedule) => {
+  return "gantt \n title A Gantt Diagram\n" +
+    "dateFormat  YYYY-MM-DD HH:mm:ss.ms \n" + Object.entries(shedule).map(([key, machine]) => {
+      const tasks = machine.tasks.map(it => `${new Date(it.dueDate).toISOString().split(":")[0]} : ${new Date(it.start).toISOString().split("Z")[0].split("T").join(" ")}, ${new Date(it.end).toISOString().split("Z")[0].split("T").join(" ")} `).join("\n")
+      return [key, tasks]
+    }).map(it => {
+      return `section ${it[0]} \n` + it[1]
+    }).join("\n")
+}
