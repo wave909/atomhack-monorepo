f960b5b27ea087e190b600af92f40d7f8b0fe149
kiri <kirillknizhov@mail.ru>
2021-08-28 15:01:51 +0700

    shedule manager demo

--- packages/web/src/app.tsx
+++ packages/web/src/app.tsx
@@ -5,12 +5,15 @@ import store from "./store";
 import {DepartmentsTree} from "./components/DepartmentsTree/DepartmentsTree";
 import './styles/index.scss'
 import ChatWidget from "./classifier-widget";
+// @ts-ignore
+import {SheduleManager} from "./shedule-manager/SheduleManager";
 
 const App = () => {
   return <BrowserRouter>
     <ReduxProvider store={store}>
       <Switch>
         <Route path="/classifier" component={ChatWidget}/>
+        <Route path="/shedule" component={SheduleManager}/>
         <Route path="/" component={DepartmentsTree}/>
       </Switch>
     </ReduxProvider>
