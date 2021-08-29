2694f9ed1b45531bd6467c99866cebf8e38a8d0a
Nikita Popov <barbaroso1346@gmail.com>
2021-08-29 16:06:11 +0700

    Task card checkbox, Employee card input

--- packages/web/src/components/EmployeeCard/EmployeeCard.tsx
+++ packages/web/src/components/EmployeeCard/EmployeeCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {Card} from "@material-ui/core";
 import style from "./EmployeeCard.module.scss";
 import {Employee} from "./employee";
@@ -6,15 +6,25 @@ import {Employee} from "./employee";
 export const EmployeeCard = ({employee, selected, onClick}:
                                { employee: Employee , selected?: boolean, onClick: () => void}) => {
 
+  const [performance, setPerformance] = useState<number>()
+
   return <Card onClick={onClick} className={`${style['employee']}`} raised={selected}>
     <div className={style['employee__photo']}/>
     <div className={style['employee__info']}>
-      <div className={style['employee__name']}> {employee.name} </div>
-      <div className={style['employee__position']}> {employee.position} </div>
+      <div className={style['title']}> {employee.name} </div>
+      <div className={style['subtitle']}> {employee.position} </div>
     </div>
-    <div className={style['task-info']}>
-      <div className={style['workload']}> Загруженность {'75%'}</div>
-      <div className={style['task-count']}>Задач в плaне: {'4'}</div>
+    <div className={style['column']}>
+      <div className={style['title']}> Загруженность {'75%'}</div>
+      <div className={style['subtitle']}>Задач в плaне: {'4'}</div>
     </div>
+    <div className={style['column']}>
+      <div className={style['title']}>Производительность:</div>
+      <input className={`${style['input']} ${style['subtitle']}`}
+             type="number"
+             value={performance}
+             onChange={(e) => setPerformance(parseFloat(e.target.value))}/>
+    </div>
+
   </Card>
 }
